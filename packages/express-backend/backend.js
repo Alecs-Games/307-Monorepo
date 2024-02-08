f1f5f4a14fcf27a159f7df2206cd670a2642d38a
Alec Evans <alecevans20118@gmail.com>
2024-02-08 10:38:08 -0800

    update to assignment 4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,7 @@
 // backend.js
 import express from "express";
+import cors from "cors";
+import userServices from "./user-services.js";
 
 const app = express();
 const port = 8000;
@@ -10,35 +12,33 @@ const users = {
     {
       id: "xyz789",
       name: "Charlie",
-      job: "Janitor"
+      job: "Janitor",
     },
     {
       id: "abc123",
       name: "Mac",
-      job: "Bouncer"
+      job: "Bouncer",
     },
     {
       id: "ppp222",
       name: "Mac",
-      job: "Professor"
+      job: "Professor",
     },
     {
       id: "yat999",
       name: "Dee",
-      job: "Aspring actress"
+      job: "Aspring actress",
     },
     {
       id: "zap555",
       name: "Dennis",
-      job: "Bartender"
-    }
-  ]
+      job: "Bartender",
+    },
+  ],
 };
 
 const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
+  return users["users_list"].filter((user) => user["name"] === name);
 };
 const addUser = (user) => {
   users["users_list"].push(user);
@@ -46,6 +46,9 @@ const addUser = (user) => {
 };
 const deleteUser = (user) => {
   const index = users["users_list"].indexOf(user);
+  if (index == -1) {
+    return -1;
+  }
   users["users_list"].splice(index, 1);
   return user;
 };
@@ -53,45 +56,77 @@ app.use(express.json());
 
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
+  userServices.findUserById(id).then((user) => {
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.send(result);
+    }
+  });
 });
 
-
 app.get("/users", (req, res) => {
-  let value = users["users_list"];
   const name = req.query.name;
   const job = req.query.job;
-  if(name != undefined){
-    value = value.filter(
-      (user) => user["name"] === name);
+  userServices.getUsers(name, job).then((result) => {
+    res.send(result);
+  });
+
+  /*let value = { users_list: [] };
+  value["users_list"] = users["users_list"];
+  const name = req.query.name;
+  const job = req.query.job;
+  if (name != undefined) {
+    value["users_list"] = value["users_list"].filter(
+      (user) => user["name"] === name
+    );
   }
-  if(job != undefined){
-    value = value.filter(
-      (user) => user["job"] === job);
+  if (job != undefined) {
+    value["users_list"] = value["users_list"].filter(
+      (user) => user["job"] === job
+    );
   }
-  res.send(value)
+  res.send(value);*/
 });
 
 app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}`
-  );
+  console.log(`Example app listening at http://localhost:${port}`);
 });
 
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
-  addUser(userToAdd);
-  res.send();
+  userServices.addUser(userToAdd).then((user) => {
+    res.status(201);
+    res.send(user);
+  });
 });
 
 app.delete("/users/:id", (req, res) => {
   const id = req.params["id"];
-  let user = findUserById(id);
-  deleteUser(user)
-  res.send();
+  userServices
+    .deleteUserById(id)
+    .then((user) => {
+      if (user == undefined) {
+        res.status(404);
+      } else {
+        res.status(204);
+      }
+      res.send();
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(404);
+      res.send();
+    });
+
+  /*userServices.findUserById(id).then((user) => {
+    if (user == undefined) {
+      res.status(404);
+    } else {
+      deleteUser(user);
+      res.status(204);
+    }
+    console.log(res.status);
+    res.send();
+  });*/
 });
--- packages/react-frontend/src/index.js
+++ packages/react-frontend/src/index.js
@@ -5,7 +5,6 @@ import MyApp from "./MyApp";
 import "./index.css";
 
 const container = document.getElementById("root");
-
 // Create a root
 const root = ReactDOMClient.createRoot(container);
 
